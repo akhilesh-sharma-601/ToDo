c261bffc8ebe3cfe435ad2be0433274eba863fb5
Akhilesh Sharma <akhileshsharma601@yahoo.com>
2020-11-17 23:31:37 +0530

    Update TodoItem.js

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 
 export default class TodoItem extends Component {
   render() {
-    const { title, handleDelete, handleEdit } = this.props;
+    const { title, handleDelete, handleEdit} = this.props;
+   
     return (
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
         
-        <span className="mx-2 text-secondary" onClick={handleDelete}>
+        <span className="mx-2 text-secondary" >
             <i className="fas fa-dot-circle"></i>
           </span>
 
-        <h6>{title}</h6>
+        <h6 >{title}</h6>
         <div className="todo-icon">
           <span className="mx-2 text-primary" onClick={handleEdit}>
             <i className="fas fa-pen"></i>
